cb8a407826377e4e9a14eb33ad94d7be5269d8fe
Victor Chrisosthemos <victor_lampert@outlook.com>
2024-07-22 10:06:38 -0300

    Add files via upload

---
+++ b1015.py
@@ -0,0 +1,10 @@
+p1 = input()
+p2 = input()
+p1 = p1.split()
+p2 = p2.split()
+x1 = float(p1[0])
+y1 = float(p1[1])
+x2 = float(p2[0])
+y2 = float(p2[1])
+dist = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("{:.4f}".format(dist))
