4198fda6a3eb56c05c5fed4e7932c6cffbd31aa6
Victor Chrisosthemos <victor_lampert@outlook.com>
2024-07-30 10:23:37 -0300

    Add files via upload

---
+++ b1050.py
@@ -0,0 +1,21 @@
+n1 = int(input())
+valor = ''
+if n1 == 61:
+    valor = 'Brasilia'
+elif n1 == 71:
+    valor = 'Salvador'
+elif n1 == 11:
+    valor = 'Sao Paulo'
+elif n1 == 21:
+    valor = 'Rio de Janeiro'
+elif n1 == 32:
+    valor ='Juiz de Fora'
+elif n1 == 19:
+    valor = 'Campinas'
+elif n1 == 27:
+    valor = 'Vitoria'
+elif n1 == 31:
+    valor = 'Belo Horizonte'
+else:
+    valor = 'DDD nao cadastrado'
+print(valor)
